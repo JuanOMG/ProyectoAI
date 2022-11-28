6c45a644b96e841e5175bd57c4a16cb35d196681
Juan Bonilla <juan.bonilla.morales@udla.edu.ec>
2022-11-28 17:39:57 -0500

    SCRIPTS
    Movimiento vehículos, seguimiento de pista y agente para vehículo. Tambíen el acrhivo de configuración para entrenar las IA

---
+++ TrackTile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackTile : MonoBehaviour
+{
+    void OnDrawGizmosSelected()
+    {
+            // Dibuja una línea azul desde esta transformación hasta el objetivo.
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(
+                transform.position + Vector3.up * 2, 
+                transform.position + Vector3.up * 2 + transform.forward * 3
+                );
+        
+    }
+}
